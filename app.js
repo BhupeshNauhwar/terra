fbf5299852550098ae9572e89567c7041e6133ca
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2024-05-07 00:47:54 +0530

    commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server Started at http://localhost:3000');
+});
